514f7accf6f2eb9100e735f8d782059c2b107025
erapx <marco.giuliani@outlook.com>
2015-12-28 15:09:07 +0100

    Deepviz package

--- examples/sandbox_test.py
+++ examples/sandbox_test.py
@@ -20,10 +20,9 @@ f.close()
 
 # Download sample binary
 result = sbx.download_sample(md5="00000000000000000000000000000000",
-                             api_key="0000000000000000000000000000000000000000000000000000000000000000",
-                             path=".")
+                             api_key="0000000000000000000000000000000000000000000000000000000000000000", path=".")
 
-# Upload a sample and wait until the analysis is complete
+# Upload sample and wait until the analysis is complete
 _hash = hashlib.md5(open("file.exe", 'rb').read()).hexdigest()
 
 sbx.upload_sample(path="file.exe", api_key="0000000000000000000000000000000000000000000000000000000000000000")
