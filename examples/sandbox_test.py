c9c2874b0c6e19187c966eb449ea6e22120da4b2
erapx <marco.giuliani@outlook.com>
2015-12-28 15:17:53 +0100

    ...

--- examples/sandbox_test.py
+++ examples/sandbox_test.py
@@ -2,34 +2,33 @@ from deepviz import sandbox
 import time
 import hashlib
 
+API = "0000000000000000000000000000000000000000000000000000000000000000"
+
 sbx = sandbox.Sandbox()
 
 # Retrieve sample scan result
-result = sbx.sample_result(md5="00000000000000000000000000000000",
-                           api_key="0000000000000000000000000000000000000000000000000000000000000000")
+result = sbx.sample_result(md5="00000000000000000000000000000000", api_key=API)
 f = open("result.txt", "wb")
-f.write(str(result))
+f.write(str(result.msg))
 f.close()
 
 # Retrieve sample full scan report
-result = sbx.sample_report(md5="00000000000000000000000000000000",
-                           api_key="0000000000000000000000000000000000000000000000000000000000000000")
+result = sbx.sample_report(md5="00000000000000000000000000000000", api_key=API)
 f = open("report.txt", "wb")
-f.write(str(result))
+f.write(str(result.msg))
 f.close()
 
 # Download sample binary
-result = sbx.download_sample(md5="00000000000000000000000000000000",
-                             api_key="0000000000000000000000000000000000000000000000000000000000000000", path=".")
+sbx.download_sample(md5="00000000000000000000000000000000", api_key=API, path=".")
 
 # Upload sample and wait until the analysis is complete
 _hash = hashlib.md5(open("file.exe", 'rb').read()).hexdigest()
 
-sbx.upload_sample(path="file.exe", api_key="0000000000000000000000000000000000000000000000000000000000000000")
-result = sbx.sample_result(md5=_hash, api_key="0000000000000000000000000000000000000000000000000000000000000000")
+sbx.upload_sample(path="file.exe", api_key=API)
+result = sbx.sample_result(md5=_hash, api_key=API)
 
 while result.status != "success":
     time.sleep(30)
-    result = sbx.sample_result(md5=_hash, api_key="0000000000000000000000000000000000000000000000000000000000000000")
+    result = sbx.sample_result(md5=_hash, api_key=API)
 
 print result.msg['classification']['result']
